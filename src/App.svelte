9704ab6c40fe84ac3e710da4e5af0e1b3811bbaa
Gravtas-J <144185093+Gravtas-J@users.noreply.github.com>
2023-10-04 07:23:48 +1000

    Update App.svelte
    just did some playing around with the check box and adding some CSS elements

--- src/App.svelte
+++ src/App.svelte
@@ -13,12 +13,6 @@
 	  tasks = tasks.filter(task => task.id !== id);
 	}
   
-	function toggleDone(id) {
-	  tasks = tasks.map(task => 
-		task.id === id ? {...task, done: !task.done } : task
-	  );
-	}
-  
 	$: unfinishedCount = tasks.filter(task => !task.done).length;
   </script>
   
@@ -26,16 +20,30 @@
 	.done {
 	  text-decoration: line-through;
 	}
+  
+	ul {
+	  width: 300px; /* Or any desired width */
+	  height: 400px; /* Or any desired height */
+	  border: 2px solid #333; /* Border for the rectangle */
+	  padding: 20px; 
+	  background-color: #f4f4f4; /* Light gray background */
+	  overflow-y: auto; /* Adds scrollbar if the tasks exceed the height */
+	}
+  
+	li {
+	  margin-bottom: 10px; /* Adds some space between tasks */
+	}
   </style>
   
   <main>
 	<input bind:value={taskInput} placeholder="Enter a task" />
 	<button on:click={addTask}>Add Task</button>
   
+	<!-- This is where the list, appearing as a large rectangle, will be -->
 	<ul>
 	  {#each tasks as task (task.id)}
 		<li>
-		  <input type="checkbox" bind:checked={task.done} on:change={() => toggleDone(task.id)} />
+		  <input type="checkbox" bind:checked={task.done} />
 		  <span class={task.done ? 'done' : ''}>{task.text}</span>
 		  <button on:click={() => deleteTask(task.id)}>Delete</button>
 		</li>
