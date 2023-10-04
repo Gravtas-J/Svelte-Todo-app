e93d1326235e211d5e991fe1e799cbf6bcca6f6c
Gravtas-J <144185093+Gravtas-J@users.noreply.github.com>
2023-10-04 14:53:32 +1000

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -33,23 +33,34 @@
 	li {
 	  margin-bottom: 10px; /* Adds some space between tasks */
 	}
+	body, html {
+    margin: 0;
+    padding: 0;
+    height: 100%; /* Ensure full viewport height */
+    font-family: Arial, sans-serif; /* Optional: for better font styling */
+	}
+
+	main {
+    display: flex;
+    flex-direction: column;
+    align-items: center; /* Center horizontally */
+    justify-content: center; /* Center vertically */
+    height: 100%; /* Use the full viewport height */
+	}
   </style>
   
-  <main>
-	<input bind:value={taskInput} placeholder="Enter a task" />
-	<button on:click={addTask}>Add Task</button>
-  
-	<!-- This is where the list, appearing as a large rectangle, will be -->
-	<ul>
-	  {#each tasks as task (task.id)}
-		<li>
-		  <input type="checkbox" bind:checked={task.done} />
-		  <span class={task.done ? 'done' : ''}>{task.text}</span>
-		  <button on:click={() => deleteTask(task.id)}>Delete</button>
-		</li>
-	  {/each}
-	</ul>
-  
-	<p>{unfinishedCount} unfinished task(s)</p>
-  </main>
+<main>
+  <input bind:value={taskInput} placeholder="Enter a task" />
+  <button on:click={addTask}>Add Task</button>
+  <ul>
+    {#each tasks as task (task.id)}
+      <li>
+        <input type="checkbox" bind:checked={task.done} />
+        <span class={task.done ? 'done' : ''}>{task.text}</span>
+        <button on:click={() => deleteTask(task.id)}>🗑️</button>
+      </li>
+    {/each}
+  </ul>
+  <p>{unfinishedCount} unfinished task(s)</p>
+</main>
   
